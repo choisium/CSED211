3475d86e66bd6a759425480842ef7569fd7e7126
Soomin Choi <choisium@gmail.com>
2021-09-09 18:11:37 +0900

    lab1: add given problem codes for lab1

---
+++ lab1/bits.c
@@ -0,0 +1,59 @@
+/* CSED 211 Fall '2021.  Lab L1 */
+
+
+
+#if 0
+LAB L1 INSTRUCTIONS:
+
+#endif
+
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ */
+int bitAnd(int x, int y) {
+
+
+}
+
+/* 
+ * addOK - Determine if can compute x+y without overflow
+ *   Example: addOK(0x80000000,0x80000000) = 0,
+ *            addOK(0x80000000,0x70000000) = 1, 
+ *   Legal ops: ! ~ & ^ | + << >>
+ */
+int addOK(int x, int y) {
+
+}
+
+/* 
+ * isNegative - return 1 if x < 0, return 0 otherwise 
+ *   Example: isNegative(-1) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int isNegative(int x) {
+
+}
+
+/* 
+ * logicalShift - logical right shift of x by y bits, 1 <= y <= 31
+ *   Example: logicalShift(-1, 1) = TMax.
+ *   Legal ops: ! ~ & ^ | + << >>
+ */
+int logicalShift(int x, int y)
+{
+  
+}
+
+
+/*
+ * bitCount - returns count of number of 1's in word
+ *   Examples: bitCount(5) = 2, bitCount(7) = 3
+ *   Legal ops: ! ~ & ^ | + << >>
+ */
+int bitCount(int x) {
+
+}
