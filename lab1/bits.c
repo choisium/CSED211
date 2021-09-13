b59a5dd0245dcd3c34b325b044035c5e63220caa
Soomin Choi <choisium@gmail.com>
2021-09-13 17:48:57 +0900

    lab1: modify my solution to meet constraints

--- lab1/bits.c
+++ lab1/bits.c
@@ -24,7 +24,15 @@ int bitAnd(int x, int y) {
  *   Legal ops: ! ~ & ^ | + << >>
  */
 int addOK(int x, int y) {
-    return ((x >> 31 & 1) ^ (y >> 31 & 1)) | !((x >> 31 & 1) ^ ((x+y) >> 31 & 1));
+    int check = x ^ (x + y);
+    check |= (check >> 1) & (~((1 << 31) >> (1 + ~1 + 1)));
+    check |= (check >> 2) & (~((1 << 31) >> (2 + ~1 + 1)));
+    check |= (check >> 4) & (~((1 << 31) >> (4 + ~1 + 1)));
+    check |= (check >> 8) & (~((1 << 31) >> (8 + ~1 + 1)));
+    check |= (check >> 16) & (~((1 << 31) >> (16 + ~1 + 1)));
+    check ^= (check >> 1) & (~((1 << 31) >> (1 + ~1 + 1))) ;
+    int res = check & x;
+    return !res;
 }
 
 /* 
@@ -45,7 +53,7 @@ int isNegative(int x) {
  */
 int logicalShift(int x, int y)
 {
-    return (x >> y) & ~((~1 + 1) << (32 + ~y + 1));
+    return (x >> y) & (~((1 << 31) >> (y + ~1 + 1)));
 }
 
 
@@ -55,19 +63,27 @@ int logicalShift(int x, int y)
  *   Legal ops: ! ~ & ^ | + << >>
  */
 int bitCount(int x) {
-    int count = (x & 0x55555555) + (x >> 1 & 0x55555555);
-    count = (count & 0x33333333) + (count >> 2 & 0x33333333);
-    count = (count & 0x0f0f0f0f) + (count >> 4 & 0x0f0f0f0f);
-    count = (count & 0x00ff00ff) + (count >> 8 & 0x00ff00ff);
-    count = (count & 0x0000ffff) + (count >> 16 & 0x0000ffff);
-    return count;
+    int test_bit = 1 | (1 << 8) | (1 << 16) | (1 << 24);  // 0x01010101, 6 ops
+
+    // calculate count of 1 in every 8 bits
+    int count = x & test_bit;  //  count of 1 for last 1 bit, 1 ops
+    count += (x >> 1) & test_bit;  // count of 1 for last 2 bits, 3 ops
+    count += (x >> 2) & test_bit;  // count of 1 for last 3 bits, 3 ops
+    count += (x >> 3) & test_bit;  // count of 1 for last 4 bits, 3 ops
+    count += (x >> 4) & test_bit;  // count of 1 for last 5 bits, 3 ops
+    count += (x >> 5) & test_bit;  // count of 1 for last 6 bits, 3 ops
+    count += (x >> 6) & test_bit;  // count of 1 for last 7 bits, 3 ops
+    count += (x >> 7) & test_bit;  // count of 1 for last 8 bits, 3 ops
+
+    // get sum of 1 for all bits by adding count in every 8 bits
+    return (count + (count >> 8) + (count >> 16) + (count >> 24)) & 0xff;  // 7 ops
 }
 
 int main() {
     printf("%d\n", bitAnd(6, 5)); // 4
     printf("%d, %d\n", addOK(0x80000000,0x80000000), addOK(0x80000000,0x70000000)); // 0, 1
     printf("%d\n", isNegative(-1)); // 1
-    printf("%d %d\n", logicalShift(-6, 2), logicalShift(3, 1));
+    printf("%X %X %d\n", -6, logicalShift(-6, 2), logicalShift(3, 1)); // 3FFFFFFE 1
     printf("%d %d\n", bitCount(5), bitCount(7)); // 2, 3
 
 }
