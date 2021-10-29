e3216218950fc62a4282ff2738e0a87649fb0f07
Soomin Choi <choisium@gmail.com>
2021-10-29 22:24:49 +0900

    lab5_quiz: add quiz solutions for lab5 quiz

--- lab5_practice/quiz.c
+++ lab5_practice/quiz.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 void getbuf(){
-    int a1 = 0;
-    double b = 0;
+    int a1 = 1;
+    double b = 3;
     char buf[21];
 
     scanf("%s", buf);
---
+++ lab5_practice/quiz.s
@@ -0,0 +1,59 @@
+	.file	"quiz.c"
+	.section	.rodata
+.LC1:
+	.string	"%s"
+	.text
+	.globl	getbuf
+	.type	getbuf, @function
+getbuf:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$1, -44(%rbp)
+	movabsq	$4613937818241073152, %rax
+	movq	%rax, -40(%rbp)
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movq	-8(%rbp), %rax
+	xorq	%fs:40, %rax
+	je	.L2
+	call	__stack_chk_fail
+.L2:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	getbuf, .-getbuf
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %eax
+	call	getbuf
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-36)"
+	.section	.note.GNU-stack,"",@progbits
